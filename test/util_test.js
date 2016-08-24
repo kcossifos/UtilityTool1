3b93d0730fb5faae470078b3c8e31db66f1710d4
KJGilkes <khalilgilkes@Khalils-MacBook-Pro.local>
2016-08-24 18:35:18 -0400

    added tests for new console.log error and warn functionality

--- test/util_test.js
+++ test/util_test.js
@@ -1,6 +1,8 @@
 const expect = require('chai').expect;
 const util = require('../lib/util');
 const fs = require('fs');
+const sinon = require('sinon');
+require('mocha-sinon');
 
 describe('TestUtilDebug', () => {
   let totalChar = 0;
@@ -25,7 +27,7 @@ describe('TestUtilDebug', () => {
   });
 
   it('Should be able to append to log file when status is not defined', (done) => {
-    util.debug({ methods: '"Hello without status"' }, 30);
+    util.log({ methods: '"Hello without status"' }, 30);
     const newCharlength = fs.readFileSync(path).length;
     expect(newCharlength).to.be.above(totalChar);
     totalChar = newCharlength;
@@ -33,7 +35,7 @@ describe('TestUtilDebug', () => {
   });
 
   it('Should be able to append to log file when status is defined', (done) => {
-    util.debug({ methods: '"Hello with status"' }, 35, 2);
+    util.log({ methods: '"Hello with status"' }, 35, 2);
     const newCharlength = fs.readFileSync(path).length;
     expect(newCharlength).to.be.above(totalChar);
     totalChar = newCharlength;
@@ -42,9 +44,31 @@ describe('TestUtilDebug', () => {
 
   it('Should not be able to append to log file when NODE_ENV is not defined', (done) => {
     process.env.NODE_ENV = '';
-    util.debug({ methods: '"Hello with process.env.NODE_ENV not defined" ' }, 43, 2);
+    util.log({ methods: '"Hello with process.env.NODE_ENV not defined" ' }, 43, 2);
     const newCharlength = fs.readFileSync(path).length;
     expect(newCharlength).to.be.equal(totalChar);
+    done();
+  });
+
+  it('Should be able to console.log', (done) => {
+    const logfunc = sinon.spy();
+    logfunc(util.log('Logged stuff.'));
+    expect(logfunc.callCount).to.equal(1);
+    done();
+  });
+
+  it('Should be able to console.warn', (done) => {
+    const warnfunc = sinon.spy();
+    warnfunc(util.warn('Logging'));
+    expect(warnfunc.callCount).to.equal(1);
+    done();
+  });
+
+  it('Should be able to console.error', (done) => {
+    process.env.NODE_ENV = '';
+    const errfunc = sinon.spy();
+    errfunc(util.error('Logging'));
+    expect(errfunc.callCount).to.equal(1);
     done();
   });
 });
