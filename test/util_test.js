299ccfbe17bcb55fcd8eeb7de51d8860327fc009
KJGilkes <khalilgilkes@Khalils-MacBook-Pro.local>
2016-08-24 19:54:03 -0400

    Changed test statements

--- test/util_test.js
+++ test/util_test.js
@@ -6,21 +6,21 @@ require('mocha-sinon');
 describe('TestUtilDebug', () => {
   it('Should be able to console.log', (done) => {
     const logfunc = sinon.spy();
-    logfunc(util.log('Logged stuff.'));
+    logfunc(util.log('I have just console logged.'));
     expect(logfunc.callCount).to.equal(1);
     done();
   });
 
   it('Should be able to console.warn', (done) => {
     const warnfunc = sinon.spy();
-    warnfunc(util.warn('Logging'));
+    warnfunc(util.warn('I have just console warned'));
     expect(warnfunc.callCount).to.equal(1);
     done();
   });
 
   it('Should be able to console.error', (done) => {
     const errfunc = sinon.spy();
-    errfunc(util.error('Logging'));
+    errfunc(util.error('I have just console errored'));
     expect(errfunc.callCount).to.equal(1);
     done();
   });
